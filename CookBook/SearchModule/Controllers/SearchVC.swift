6421c57d13be562dda016eb80cb961b01048d58f
Yan Shvyndikov <Yansv2006@gmail.com>
2023-02-28 14:13:22 +0300

    addTable

--- CookBook/SearchModule/Controllers/SearchVC.swift
+++ CookBook/SearchModule/Controllers/SearchVC.swift
@@ -2,10 +2,40 @@
 import UIKit
 
 final class SearchVC: UIViewController {
+    
+    private let searchView = SearchView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        //title = "Search"
+        view.backgroundColor = .secondarySystemBackground
+        searchView.tableView.delegate = self
+        searchView.tableView.dataSource = self
+        setUpView()
     }
+    
+    private func setUpView() {
+        view.addSubview(searchView)
+        NSLayoutConstraint.activate([
+            searchView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            searchView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            searchView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+}
+
+extension SearchVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier, for: indexPath) as! CustomTableViewCell
+        
+        return cell
+    }
+    
+    
 }
--- CookBook/SearchModule/Views/SearchView.swift
+++ CookBook/SearchModule/Views/SearchView.swift
@@ -3,12 +3,47 @@ import UIKit
 
 final class SearchView: UIView {
     
+    private var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.searchBarStyle = UISearchBar.Style.default
+        searchBar.placeholder = "Enter ingredients"
+        searchBar.sizeToFit()
+        searchBar.isTranslucent = false
+        searchBar.backgroundImage = UIImage()
+        return searchBar
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = 180
+        tableView.layer.cornerRadius = 10
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: CustomTableViewCell.identifier)
+        addSubviews(searchBar, tableView)
+        addConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 10),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -27),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+        ])
     }
 }
