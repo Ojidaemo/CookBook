aee438cc5b5a51a468b1f2517aad22a556e2ea80
Yan Shvyndikov <Yansv2006@gmail.com>
2023-02-28 15:11:17 +0300

    AddTableView

---
+++ CookBook/Cells/SearchTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  SearchTableViewCell.swift
+//  CookBook
+//
+//  Created by Yan Shvyndikov on 28.02.2023.
+//
+
+import Foundation
---
+++ CookBook/Screens/Views/SearchView.swift
@@ -0,0 +1,8 @@
+//
+//  SearchView.swift
+//  CookBook
+//
+//  Created by Yan Shvyndikov on 28.02.2023.
+//
+
+import Foundation
