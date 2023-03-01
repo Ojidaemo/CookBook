65649a218a803a0ae87d8411ac01d3f4a5846341
Nikolay Dobychin <koddek@bk.ru>
2023-03-01 21:33:21 +0300

    Created ContentView and add scrollView
    add UI

--- CookBook/Screens/DetailedRecipe/Controller/DetailedRecipeViewController.swift
+++ CookBook/Screens/DetailedRecipe/Controller/DetailedRecipeViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 
 class DetailedRecipeViewController: UIViewController {
     
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    let contentView = ContentView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .brown
+        setupView()
+        setConstraints()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .secondarySystemBackground
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
     }
 }
+
+extension DetailedRecipeViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -1000)
+        ])
+    }
+}
+
